b6cfb58445802014083d817d7018dd0c13dcbb87
Zach bylhouwer <43009020+Zombienorman0@users.noreply.github.com>
2019-09-24 13:07:03 -0600

    down

--- Script/Array.js
+++ Script/Array.js
@@ -1,2 +1,35 @@
 //console.log("did you change my file name monsieur?")
 console.log("you made it to the console! Congrats!")
+
+console.log('');// prints two spaces in console
+console.log('');// output, which is easier to read
+
+let person1 = {
+firstname : "Zach",
+lastName: "Bylhouwer",
+age: 15,
+favoriteNumber: 7,
+hair: "Dark Brown",
+male: true,
+female: false,
+favoriteGame: "Smite",
+favoriteColor: "Purple",
+favoriteSentenceToHearOrSay: "Say No More Mr. Mercer",
+}
+
+let person2 = {
+firstname:  "Athena" ,
+lastName: "Chow",
+age: 16,
+favoriteNumber: 69,
+hair: "Brunette",
+male: false,
+female: true,
+favoriteGame: "Overwatch",
+favoriteColor: "Blue",
+favoriteSentenceToHearOrSay: "what can you do?",
+};
+
+
+console.log(person2);
+console.log(person1);
